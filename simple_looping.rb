7134610d578a91b171b4290ef9274c1a775c53cf
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-16 05:44:47 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,10 +1,10 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times) 
-  loop do 
+def loop_iterator(number_of_times)
+  loop do
     number_of_times = number_of_times + 1
     puts "Welcome to Flatiron School's Web Development Course!"
-   
+
     if counter >= 10 # If our counter is 10 or more
       break # Stop the loop
     end
